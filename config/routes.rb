77fffa847b8a1f0bf957e2349c1878cc5fab2881
Claire Lin <chunhuilin7516@gmail.com>
2022-10-06 00:04:14 +0800

    add create method to controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tasks#index'
   get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
-
   end
