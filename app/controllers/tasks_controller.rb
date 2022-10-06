3fe47ec1314271f5c167a44bcbc101ef85d890e5
Claire Lin <chunhuilin7516@gmail.com>
2022-10-06 17:33:03 +0800

    refactoring & simple frontend

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    set_task
   end
 
   def new
@@ -18,17 +19,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = set_task
   end
 
   def update
-    task = set_task
-    task.update(task_params)
-    redirect_to task_path(task)
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
-    @task = set_task
     @task.destroy
     redirect_to root_path, status: :see_other
   end
